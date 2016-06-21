44b5aa8a4ba84132915ba5e1d436218224f3a3d1
= <thiagorthomaz@gmail.com>
2016-06-21 14:07:10 -0300

    Update slimAlert

--- client/js/directives/slimAlert.js
+++ client/js/directives/slimAlert.js
@@ -1,10 +1,26 @@
 app.directive("slimAlert", function(){
   return {
     templateUrl: "partials/slimAlert.html",
+    controller : "SlimAlertCtrl",
     scope : {
       title : "@",
       message : "@",
       type : "@"//info, warning, danger, success
     }
   };
+});
+
+app.controller("SlimAlertCtrl", function($scope){
+  
+  $scope.isInfo = $scope.type === 'info';
+  $scope.isWarning = $scope.type === 'warning';
+  $scope.isDanger = $scope.type === 'danger';
+  $scope.isSuccess = $scope.type === 'success';
+  
+  $scope.show = true;
+  
+  $scope.close = function() {
+    $scope.show = false;
+  };
+  
 });
