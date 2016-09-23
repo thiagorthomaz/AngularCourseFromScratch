ece4a99b19778caf7334ea89dc393dafe13cea51
= <thiagorthomaz@gmail.com>
2016-09-23 08:51:02 -0300

    Dumb stress test

--- client/js/app.js
+++ client/js/app.js
@@ -1,20 +1,34 @@
 
 var app = angular.module("phonebook", ['slimAlert', 'ui.router']);
 
-app.config(function($stateProvider) {
+app.config(function($stateProvider,$urlRouterProvider) {
+  
+  $urlRouterProvider.otherwise("/default/phonebook");
+  
+  var defaultState = {
+    name: 'default',
+    url: '/default',
+    abstract : true,
+    views: {
+      default: {
+        templateUrl: "partials/default.html"
+      }
+    }
+  };
   
   var phonebookState = {
-    name: 'phonebook',
+    name: 'default.phonebook',
     url: '/phonebook',
     templateUrl: 'partials/phonebook.html'
   };
 
   var detailsState = {
-    name: 'details',
+    name: 'default.details',
     url: '/details/{id}',
     templateUrl: 'partials/details.html'
   };
 
+  $stateProvider.state(defaultState);
   $stateProvider.state(phonebookState);
   $stateProvider.state(detailsState);
 
--- client/js/controllers/detailsCtrl.js
+++ client/js/controllers/detailsCtrl.js
@@ -1,5 +1,9 @@
-app.controller("detailsCtrl", function ($scope, OperatorAPI) {
+app.controller("detailsCtrl", function ($scope, OperatorAPI, ContactsAPI) {
   
+  /**************************************************************
+   * Just want to test how render a large page
+   **************************************************************/
+
   var list_details_test = new Array();
   list_details_test.push({id : list_details_test.length +1 });
   list_details_test.push({id : list_details_test.length +1 });
@@ -48,9 +52,66 @@ app.controller("detailsCtrl", function ($scope, OperatorAPI) {
   
   $scope.list = list_details_test;
   
-  
   OperatorAPI.getOperators().success(function (data, status) {
     $scope.operators = data;
   });
   
+  ContactsAPI.getContacts().success(function (data, status) {
+      var result = [];
+      for (var i in data) {
+         result.push(data[i]);
+      }
+
+      $scope.contacts1 = result;
+  }).error(function (data, status) {
+    if (!data) {
+      $scope.error = "Error: Unable to load data.";
+    } else {
+      $scope.error = "Error: " + data;
+    }
+  });
+  ContactsAPI.getContacts().success(function (data, status) {
+      var result = [];
+      for (var i in data) {
+         result.push(data[i]);
+      }
+
+      $scope.contacts2 = result;
+  }).error(function (data, status) {
+    if (!data) {
+      $scope.error = "Error: Unable to load data.";
+    } else {
+      $scope.error = "Error: " + data;
+    }
+  });
+  ContactsAPI.getContacts().success(function (data, status) {
+      var result = [];
+      for (var i in data) {
+         result.push(data[i]);
+      }
+
+      $scope.contacts3 = result;
+  }).error(function (data, status) {
+    if (!data) {
+      $scope.error = "Error: Unable to load data.";
+    } else {
+      $scope.error = "Error: " + data;
+    }
+  });
+  ContactsAPI.getContacts().success(function (data, status) {
+      var result = [];
+      for (var i in data) {
+         result.push(data[i]);
+      }
+
+      $scope.contacts4 = result;
+  }).error(function (data, status) {
+    if (!data) {
+      $scope.error = "Error: Unable to load data.";
+    } else {
+      $scope.error = "Error: " + data;
+    }
+  });
+  
+
 });
